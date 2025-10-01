c5c41534ef8c5e188020076e612c55d0f3be3d70
Suraj Mane <103933043+Surajmane12@users.noreply.github.com>
2025-10-01 11:00:36 +0530

    Update dbConfig.js

--- Backend/config/dbConfig.js
+++ Backend/config/dbConfig.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database:process.env.DB_NAME
+  host: 'localhost',
+  user:'root',
+  password:'root123',
+  database:'yourhr'
 });
 
 connection.connect((err) => {
@@ -17,4 +17,5 @@ connection.connect((err) => {
   console.log('Connected to the database as id ' + connection.threadId);
 });
   
+
 module.exports = connection;
